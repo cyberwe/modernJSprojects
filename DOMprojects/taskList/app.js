73189586cc14caaa15fee7fe1d73d823efb0e2a1
AbdulMalik <abdsh2020@yahoo.com>
2020-09-04 22:47:22 +0500

    Sstore tasks in LS

--- DOMprojects/taskList/app.js
+++ DOMprojects/taskList/app.js
@@ -57,8 +57,27 @@ function addTask(e){
     li.appendChild(link);
     taskList.appendChild(li);
 
+    // Store task in LS
+    storeTaskInLocalStorage(taskInput.value);
+
+
     // Clear input
     taskInput.value="";
+}
+
+// Store task
+
+function storeTaskInLocalStorage(){
+    let tasks;
+    if(localStorage.getItem("tasks")===null){
+        tasks=[];
+    }else{
+        tasks=JSON.parse(localStorage.getItem("tasks"));
+    }
+    
+    tasks.push(task);
+
+    localStorage.setItem("tasks",JSON.stringify(tasks));
 }
 
 // Remove task
